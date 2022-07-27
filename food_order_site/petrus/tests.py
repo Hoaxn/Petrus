85cf0e61445d555d43cfb44b9e56d2183894c98f
Hoaxn <rockemu77@gmail.com>
2022-07-27 16:33:55 +0545

    removed unnecessary lines of code

--- food_order_site/petrus/tests.py
+++ food_order_site/petrus/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from petrus.views import index
 
 # Create your tests here.
+
+
+class indexTest(TestCase):
+    def test_reservation_form_resolve(self):
+        url = reverse(index)
+        self.assertEquals(resolve(url).func, index)
